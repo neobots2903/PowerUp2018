f5611b1f778bbea5c81cbbc3fc7ad3788d6efe6b
Jared-Swislow <jayswis2002@gmail.com>
2018-01-12 17:25:46 -0800

    Did the Gyro

--- PowerUp2018/src/org/usfirst/frc/team2903/robot/subsystems/Gyro2903.java
+++ PowerUp2018/src/org/usfirst/frc/team2903/robot/subsystems/Gyro2903.java
@@ -2,14 +2,85 @@ package org.usfirst.frc.team2903.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import org.usfirst.frc.team2903.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.SPI;
+
 /**
  *
  */
 public class Gyro2903 extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+	public ADXRS450_Gyro spi;
+	public AnalogGyro gyro;
+	
+	public double gyroAngle;
+	
+	//Initializing ANALOG and SPI enumerators
+	public enum GYRO_TYPE {
+		ANALOG, SPI
+	};
+	
+	
+	public Gyro2903(GYRO_TYPE gyroType) {
+		switch(gyroType) {
+			case ANALOG:
+				gyro = new AnalogGyro(RobotMap.AnalogGyro); //Sets gyro to the Hardware Map gyro
+				break;
+			
+			case SPI:
+				spi = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
+				break;
+		}
+	}
+	
+	public double gyroPosition(GYRO_TYPE gyroType) {
+		
+		switch(gyroType) {
+		case ANALOG:
+			gyro.getAngle(); //Sets gyro to the Hardware Map gyro
+			break;
+		
+		case SPI:
+			spi.getAngle();
+			break;
+		}
+		
+		gyroAngle = gyroAngle % 360;
+		return gyroAngle;
+	}
+	
+	public void calibrate(GYRO_TYPE gyroType) {
+		
+		switch(gyroType) {
+		case ANALOG:
+			gyro.calibrate(); //Sets gyro to the Hardware Map gyro
+			break;
+		
+		case SPI:
+			spi.calibrate();
+			break;
+		}
+		
+	}
+	
+	public void reset(GYRO_TYPE gyroType) {
+		
+		switch(gyroType) {
+		case ANALOG:
+			gyro.reset(); //Sets gyro to the Hardware Map gyro
+			break;
+		
+		case SPI:
+			spi.reset();
+			break;
+		}
+		
+		
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
